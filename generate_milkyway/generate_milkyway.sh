67a5def9610598844dbf762ea5139ff325ee7496
WSPITTS2 <wspitts2@crazypants.ece.ncsu.edu>
2024-10-14 02:18:21 -0400

    fixed missing milkyway tf and mwlib

--- generate_milkyway/generate_milkyway.sh
+++ generate_milkyway/generate_milkyway.sh
@@ -4,5 +4,5 @@
 # Generate Milkyway file using generate_milkyway.tcl
 Milkyway -tcl -nogui -galaxy -file generate_milkyway.tcl & exit
 
-cp $TOP/generate_milkyway/rtk-tech.tf $TOP/view-standard/
-cp -r $TOP/generate_milkyway/stdcells.mwlib $TOP/view-standard/
+cp $TOP/generate_milkyway/rtk-tech.tf $TOP/view-standard/rtk-tech.tf 
+cp -r $TOP/generate_milkyway/stdcells.mwlib $TOP/view-standard/stdcells.mwlib 
